319cacbb5ce07eefa43e28a98c4bbbf44312b67e
jobigboss <67093021+jobigboss@users.noreply.github.com>
2020-06-22 12:31:23 +0700

    Add files via upload

---
+++ Exercise4_Surasit_P.py
@@ -0,0 +1,13 @@
+FE_n = "Foundation English"
+GB_n = "General Business"
+ICS_n ="Introduction to Computer Systems"
+CP_n = "Computer Programming"
+FE_s = 56
+GB_s = 80
+ICS_s = 60.50
+CP_s = 75
+print("-------------Score------------")
+print(FE_n,":",FE_s)
+print(GB_n,":",GB_s)
+print(ICS_n,":",ICS_s)
+print(CP_n,":",CP_s)
