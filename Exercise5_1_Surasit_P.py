d97dc6ce941ca67f33b80f6b1d0a6eef623e42d9
jobigboss <67093021+jobigboss@users.noreply.github.com>
2020-06-22 20:45:23 +0700

    Exercise5_Surasit  Prasertsri

---
+++ Exercise5_1_Surasit_P.py
@@ -0,0 +1,6 @@
+num_1 = int(input("Number 1 "))
+num_2 = int(input("Number 2"))
+print(num_1,"+", num_2,"=",num_1+num_2)
+print(num_1,"-", num_2,"=",num_1-num_2)
+print(num_1,"*", num_2,"=",num_1*num_2)
+print(num_1,"/", num_2,"=",num_1/num_2)
---
+++ Exercise5_2_Surasit_p.py
@@ -0,0 +1,4 @@
+distance = int(input("ระยะทาง (km) : "))
+time = int(input("เวลาที่เคลื่อนที่ (hr) : "))
+summry = distance/time
+print(int(summry),"km/h")
